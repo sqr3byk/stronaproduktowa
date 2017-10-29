7788498d7f1cffb73c27af52af87ccdc7933becc
Grzesiek <wiecek.grzegorz@op.pl>
2017-10-29 02:12:35 +0000

    changed extension and updated index and specs to match new szablon.php

---
+++ specs.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Czyściciel próżni - parametry techniczne">
+
+    <title>Parametry techniczne | Czyściciel próżni</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <style>
+      body {
+        padding-top: 54px;
+      }
+      @media (min-width: 992px) {
+        body {
+          padding-top: 56px;
+        }
+        #specs_tables li {
+          width: 47.5%;
+          float: left;
+        }
+        #specs_tables li:first-of-type {
+          margin-right: 5%;
+        }
+      }
+      .headings {
+        background: #343a40;
+        color: white;
+      }
+      .headings td {
+        padding: .4rem !important;
+      }
+      tr:not(.headings) td {
+        padding: .6rem !important;
+      }
+      .descriptions {
+        font-style: italic;
+        color: #83989b;
+      }
+      table {
+        margin-bottom: 2em !important;
+      }
+      .feature-set td:first-of-type {
+        font-weight: bold;
+        text-align: left;
+      }
+
+    </style>
+
+  </head>
+
+  <body>
+
+    <?php
+      include $_SERVER['DOCUMENT_ROOT'] . "/stronaproduktowa/nav.html";
+    ?>
+    <!-- Page Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 text-center">
+          <h1 class="mt-5">Czyściciel próżni - dane techniczne</h1>
+          <p style="margin-bottom:3em;" class="lead">
+            Features a combo brush that is uniquely designed to pick up pet hair—and is quieter, too, especially on hard floors like wood and tile. Patented laser-guided technology scans and maps the room, plans, and methodically cleans, automatically moving from room to room. 50% larger brush, bigger-is-better dirt bin, extra large filter, and precise edge cleaning brush pick up more dirt and debris. Schedule daily cleanings or push a button for instant multi-room or spot cleaning. Automatically goes back to charge base, then returns to where it left off.
+          </p>
+          <ul id="specs_tables" class="list-unstyled">
+            <li>
+              <table class="table comparison-chart">
+                <tbody>
+                  <tr class="headings">
+                    <td class="neato" colspan="3">Product Dimensions</td>
+                  </tr>
+                  <tr class="descriptions">
+                    <td>&nbsp;</td>
+                    <td>Metric</td>
+                    <td>U.S. Standard</td>
+                  </tr>
+                  <tr class="feature-set hoverable-row">
+                    <td>Length</td>
+                    <td>32.1 cm</td>
+                    <td>12.7 in</td>
+                  </tr>
+                  <tr class="feature-set hoverable-row">
+                    <td>Width</td>
+                    <td>33.5 cm</td>
+                    <td>13.2 in</td>
+                  </tr>
+                  <tr class="feature-set hoverable-row">
+                    <td>Height</td>
+                    <td itemprop="height">10 cm</td>
+                    <td>3.9 in</td>
+                  </tr>
+                  <tr class="feature-set hoverable-row">
+                    <td>Weight</td>
+                    <td itemprop="weight">4.1 kg</td>
+                    <td>9 lbs</td>
+                  </tr>
+                </tbody>
+              </table>
+            </li>
+            <li>
+              <table class="table comparison-chart">
+                <tbody>
+                  <tr class="headings">
+                    <td colspan="2" class="neato">Technical</td>
+                  </tr>
+                  <tr class="feature-set hoverable-row">
+                    <td>Battery</td>
+                    <td>NiMH (nickel metal hydride)</td>
+                  </tr>
+                  <tr class="feature-set hoverable-row">
+                    <td>Charger Voltage</td>
+                    <td>110V, 220V</td>
+                  </tr>
+                </tbody>
+              </table>
+            </li>
+            <li>
+              <table class="table comparison-chart">
+                <tbody>
+                  <tr class="headings">
+                    <td colspan="3" class="neato">Warranty Terms</td>
+                  </tr>
+                  <tr class="feature-set">
+                    <td>Robot</td>
+                    <td colspan="2">1 Year Limited</td>
+                  </tr>
+                  <tr class="feature-set">
+                    <td>Battery</td>
+                    <td>6 Month Limited</td>
+                  </tr>
+                </tbody>
+              </table>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>

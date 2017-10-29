2b634c638cd30cb528e5a0c10589a9bc6d205b46
Grzesiek <wiecek.grzegorz@op.pl>
2017-10-29 18:33:06 +0000

    centered ugly logo

--- nav.php
+++ nav.php
@@ -7,7 +7,7 @@
         <a style="padding: 0;" class="navbar-brand" href="start.php">
           <img style="height: 2em"  src="logo.png">
           T-1000-(4Melmak)
-          <img style="height: 0.5em" src="http://pp5.dydaktyka.jkan.pl/_images/acme.png">
+        <img style="height: 0.5em; position: absolute; left: 0; right: 0; top: 0; bottom: 0; margin: auto;" src="http://pp5.dydaktyka.jkan.pl/_images/acme.png">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>

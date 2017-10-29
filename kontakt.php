6f6a4bb225736414ea679fb019fe73e93d2e3a2c
Mateusz Wielgus <mwmatty1@gmail.com>
2017-10-29 14:23:51 +0000

    added kontakt title

--- kontakt.php
+++ kontakt.php
@@ -37,7 +37,7 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-12 text-center">
-          <h1 class="mt-5">A Bootstrap 4 Starter Template</h1>
+          <h1 class="mt-5">Skontaktuj sie z nami, jak Ci wygodnie!</h1>
           <p class="lead">Complete with pre-defined file paths and responsive navigation!</p>
           <ul class="list-unstyled">
             <li>Bootstrap 4.0.0-beta</li>

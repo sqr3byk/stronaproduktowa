a6441dd152a5d102d902443302ae887283495c81
Michał Starski <mstarski95@gmail.com>
2017-10-29 16:02:52 +0000

    main page general cleanup and update

--- start.php
+++ start.php
@@ -1,56 +1,48 @@
 <!DOCTYPE html>
-<html lang="en">
 
-  <head>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>O nas | Czyściciel Próżni</title>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <!-- Bootstrap core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <title>Bare - Start Bootstrap Template</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <style>
-      body {
-        padding-top: 54px;
-      }
-      @media (min-width: 992px) {
-        body {
-          padding-top: 56px;
-        }
-      }
-
-    </style>
-
-  </head>
-
-  <body>
-
-    <?php
-      include $_SERVER['DOCUMENT_ROOT'] . "/stronaproduktowa/nav.php";
-    ?>
-    <!-- Page Content -->
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12 text-center">
-          <h1 class="mt-5">A Bootstrap 4 Starter Template</h1>
-          <p class="lead">Complete with pre-defined file paths and responsive navigation!</p>
-          <ul class="list-unstyled">
-            <li>Bootstrap 4.0.0-beta</li>
-            <li>jQuery 3.2.1</li>
-          </ul>
+        <!-- Custom styles for this template -->
+        <style>
+            body {
+                padding-top: 54px;
+            }
+            @media (min-width: 992px) {
+                body {
+                    padding-top: 56px;
+                }
+            }
+        </style>
+    </head>
+    <body>
+        <!-- Navigation bar -->  
+        <?php
+            include $_SERVER['DOCUMENT_ROOT'] . "/stronaproduktowa/nav.php";
+        ?>
+        <!-- Page Content -->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h1 class="mt-5">O nas</h1>
+                    <p class="lead">
+                        Każda marka ma swoją historię. Nasza rozpoczęła się w 2015 r. z chwilą stworzenia naszego pierwszego intelignetnego odkurzacza. 
+                        Od tego czasu nieustannie rozwijamy się, dzięki czemu jesteśmy ekspertami w tej branży.
+                    </p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  </body>
-
+        <!-- Bootstrap core JavaScript -->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    </body>
 </html>

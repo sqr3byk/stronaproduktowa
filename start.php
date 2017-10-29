fc29ce4b960a534e11dee5d5d379fbeadb3e888f
Michał Starski <mstarski95@gmail.com>
2017-10-29 17:00:22 +0000

    updated main page

--- start.php
+++ start.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="">
-        <title>O nas | Czyściciel Próżni</title>
+        <title>Strona startowa | Czyściciel Próżni</title>
 
         <!-- Bootstrap core CSS -->
         <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -21,6 +21,9 @@
                     padding-top: 56px;
                 }
             }
+            .img-responsive {
+                width: 100%;
+            }
         </style>
     </head>
     <body>
@@ -32,13 +35,32 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h1 class="mt-5">O nas</h1>
+                    <h1>O nas</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6">
+                    <img class="img-responsive" src="odkurzacz.jpg" alt="Inteligentny odkurzacz">
+                </div>
+                <div class="col-lg-6 text-justify">
                     <p class="lead">
                         Każda marka ma swoją historię. Nasza rozpoczęła się w 2015 r. z chwilą stworzenia naszego pierwszego intelignetnego odkurzacza. 
                         Od tego czasu nieustannie rozwijamy się, dzięki czemu jesteśmy ekspertami w tej branży.
                     </p>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-lg-6 text-justify">
+                    <p class="lead">
+                        Łączymy nasze dwie główne kompetencje – empatię i technologię, co pozwala nam lepiej rozumieć oczekiwania i potrzeby Klientów. 
+                        Słuchamy ich pragnień i rozumiemy obawy związane z zakupem inteligentnego odkurzacza. Pozostajemy też otwarci na ich sugestie 
+                        oferując najbardziej optymalne rozwiązania zapewniające 100% satysfakcji z zakupów.
+                    </p>
+                </div>
+                <div class="col-lg-6">
+                    <img class="img-responsive" src="okejka.jpg" alt="Rekomenduję">
+                </div>
+            </div>     
         </div>
 
         <!-- Bootstrap core JavaScript -->
